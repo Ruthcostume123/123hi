68013790f8ddd26b5110d547e4c047c54e66d464
Ruthcostume123 <60449953+Ruthcostume123@users.noreply.github.com>
2020-01-29 18:09:56 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 
 window.addEventListener("keydown", event => {
  let colorIndex = Math.random()
- let color = "red"
+ let color = "purple"
  if (colorIndex < 0.5) {
- 	color = "blue"
+ 	color = "orange"
  }
- ctx.fillSyle = color
+ ctx.fillStyle = color
 
- let x = Math.random()* 500
- let y = Math.random()* 500
+ let x = Math.random()* canvas.width
+ let y = Math.random()* canvas.height
   new Shape (x,y,200,100).draw()
 
 })
